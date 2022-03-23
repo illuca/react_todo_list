b4d4ce22d87b1b89663e24104818dc650eaac71f
illuca <xillucax@gmail.com>
2022-03-23 17:38:59 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	3_配置代理_server/carServer.js
    #	3_配置代理_server/server1.js
    #	3_配置代理_server/studentServer.js
    #	3_配置代理_src/index.jsx
    #	package.json
    #	server/server1.js
    #	src/App.jsx
    #	src/index.jsx
    #	src/setupProxy.js

---
+++ 4_github实例fetch_src/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Search from "./component/Search"
+import "./App.css"
+import CardList from "./component/CardList";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <Search/>
+                <CardList/>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import React, {Component} from 'react';
-import Search from "./component/Search"
-import "./App.css"
-import CardList from "./component/CardList";
+import {BrowserRouter, Link, Route} from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+
 
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <Search/>
-                <CardList/>
+            <div id="root">
+                <div>
+                    <div className="row">
+                        <div className="col-xs-offset-2 col-xs-8">
+                            <div className="page-header"><h2>React Router Demo</h2></div>
+                        </div>
+                    </div>
+                    <BrowserRouter>
+                        <div className="row">
+                            <div className="col-xs-2 col-xs-offset-2">
+                                <div className="list-group">
+                                    {/*<a className="list-group-item" href="./about.html">About</a>*/}
+                                    {/*<a className="list-group-item active" href="./home.html">Home</a>*/}
+                                    <Link className="list-group-item" to="/about">about</Link>
+                                    <Link className="list-group-item" to="/home">home</Link>
+                                </div>
+                            </div>
+                            <div className="col-xs-6">
+                                <div className="panel">
+                                    <div className="panel-body">
+                                        <Route path="/about" component={About}></Route>
+                                        <Route path="/home" component={Home}></Route>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </BrowserRouter>
+
+                </div>
             </div>
         );
     }
