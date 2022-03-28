a0827c8668aef208c0348fcf658df93b4bdac09a
illuca <xillucax@gmail.com>
2022-03-28 22:51:09 +0800

    switch加快匹配速度 NavLink有activeClassName,自定义高亮颜色

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
+import {NavLink, Redirect, Route} from "react-router-dom";
+import News from "./News";
+import Message from "./Message";
 
 class Home extends Component {
     render() {
         return (
-            <h3>我是Home的内容</h3>
+            <div>
+                <h3>我是Home的内容</h3>
+                <div>
+                    <ul className="nav nav-tabs">
+                        <li>
+                            <NavLink activeClassName={"my-highlight"} className={"list-group-item"}
+                                     to={"/home/news"}>News</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className={"list-group-item"} to={"/home/message"}>Message</NavLink>
+                        </li>
+                    </ul>
+                    <switch>
+                        <Route path={"/home/news"} component={News}/>
+                        <Route path={"/home/message"} component={Message}/>
+                        <Redirect to={"/home/news"}/>
+                    </switch>
+                </div>
+            </div>
         );
     }
 }
